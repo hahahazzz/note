d2fb49534425f15de30a9d5a2c827987bbf25995
qiugang <1607868475@qq.com>
2017-09-21 20:08:02 +0800

    update WaveView

--- Android/View/WaveView.java
+++ Android/View/WaveView.java
@@ -1,5 +1,6 @@
 package com.dmh.akotlin.view;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -10,12 +11,11 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.graphics.Shader;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
 /**
  * Created by QiuGang on 2017/9/20 19:22
@@ -41,9 +41,9 @@ public class WaveView extends View {
     /** 绘制外部线条的颜色 */
     private int lineColor = Color.GREEN;
     /** 前景波浪的颜色 */
-    private int foregroundWaveColor = Color.BLUE;
+    private int foregroundWaveColor = Color.parseColor("#0099ff");
     /** 背景波浪的颜色 */
-    private int backgroundWaveColor = Color.GREEN;
+    private int backgroundWaveColor = Color.parseColor("#770099ff");
     /** 前景波浪左侧相对于屏幕左侧偏移量 */
     private int foregroundWaveStartXOffset = 0;
     /** 背景波浪左侧相对于屏幕左侧偏移量 */
@@ -55,7 +55,7 @@ public class WaveView extends View {
     /** 当前水位高度 */
     private float waveLevel = 0.3f;
     /** 波浪最低点和最高点相对于基准线的偏移量 */
-    private float foregroundWaveHeight = 0.13f;
+    private float foregroundWaveHeight = 0.1f;
     private float backgroundWaveHeight = 0.11f;
     /** 外部线条宽度 */
     private float lineWidth = 30f;
@@ -74,18 +74,10 @@ public class WaveView extends View {
     /** 文本内容 */
     private String centerText = "65000";
     /** 前景和背景波浪线的Path */
-    private Path foregroundWavePath = new Path();
-    private Path backgroundWavePath = new Path();
+    private Path foregroundWavePath;
+    private Path backgroundWavePath;
 
-    private Handler animHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            foregroundWaveStartXOffset += perWaveWidth / 20;
-            backgroundWaveStartXOffset += perWaveWidth / 25;
-            resetWaveStartXOffset(foregroundWaveStartXOffset >= 0, backgroundWaveStartXOffset >= 0);
-            invalidate();
-        }
-    };
+    private boolean drawBackgroundWave = true;
 
     public WaveView(Context context) {
         this(context, null);
@@ -98,6 +90,8 @@ public class WaveView extends View {
     public WaveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setLayerType(LAYER_TYPE_HARDWARE, null);
+        foregroundWavePath = new Path();
+
         outerLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         outerLinePaint.setColor(lineColor);
         outerLinePaint.setStrokeCap(Paint.Cap.ROUND);
@@ -111,51 +105,56 @@ public class WaveView extends View {
         foregroundWavePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         foregroundWavePaint.setStyle(Paint.Style.FILL_AND_STROKE);
         foregroundWavePaint.setColor(foregroundWaveColor);
-
-        backgroundWavePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        backgroundWavePaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        backgroundWavePaint.setColor(backgroundWaveColor);
+        if (drawBackgroundWave) {
+            backgroundWavePath = new Path();
+            backgroundWavePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            backgroundWavePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+            backgroundWavePaint.setColor(backgroundWaveColor);
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //canvas.drawBitmap(foregroundWaveBitmap, 0, 0, null);
         drawInnerHalfWave(canvas);
         drawBottomLine(canvas);
         drawOuterHalfCircle(canvas);
         drawCenterText(canvas);
-        animHandler.sendEmptyMessageDelayed(0, 25);
     }
 
     private void drawInnerHalfWave(Canvas canvas) {
         foregroundWavePath.reset();
-        backgroundWavePath.reset();
         float waveY = getHeight() * (lineMarginTop - waveLevel) + lineWidth / 2;
         float foregroundWaveHeightOffset = perWaveWidth * foregroundWaveHeight;
         float backgroundWaveHeightOffset = perWaveWidth * backgroundWaveHeight;
+        foregroundWavePath.moveTo(foregroundWaveStartXOffset, waveY);
+        if (drawBackgroundWave) {
+            backgroundWavePath.reset();
+            backgroundWavePath.moveTo(backgroundWaveStartXOffset, waveY);
+        }
         for (int i = 0; i < waveCount * 2; i++) {
             float foregroundWaveStartX = foregroundWaveStartXOffset + perWaveWidth * i;
-            foregroundWavePath.moveTo(foregroundWaveStartX, waveY);
-            foregroundWavePath.cubicTo(foregroundWaveStartX + perWaveWidth / 4, waveY - foregroundWaveHeightOffset,
-                    foregroundWaveStartX + perWaveWidth / 2, waveY + foregroundWaveHeightOffset, foregroundWaveStartX +
-                            perWaveWidth, waveY);
-
-            float backgroundWaveStartX = backgroundWaveStartXOffset + perWaveWidth * i;
-            backgroundWavePath.moveTo(backgroundWaveStartX, waveY);
-            backgroundWavePath.cubicTo(backgroundWaveStartX + perWaveWidth / 4, waveY - backgroundWaveHeightOffset,
-                    backgroundWaveStartX + perWaveWidth / 2, waveY + backgroundWaveHeightOffset, backgroundWaveStartX +
-                            perWaveWidth, waveY);
+            foregroundWavePath.quadTo(foregroundWaveStartX + perWaveWidth / 4, waveY - foregroundWaveHeightOffset,
+                    foregroundWaveStartX + perWaveWidth / 2, waveY);
+            foregroundWavePath.quadTo(foregroundWaveStartX + perWaveWidth / 4 * 3, waveY + foregroundWaveHeightOffset,
+                    foregroundWaveStartX + perWaveWidth, waveY);
+            if (drawBackgroundWave) {
+                float backgroundWaveStartX = backgroundWaveStartXOffset + perWaveWidth * i;
+                backgroundWavePath.quadTo(backgroundWaveStartX + perWaveWidth / 4, waveY - backgroundWaveHeightOffset,
+                        backgroundWaveStartX + perWaveWidth / 2, waveY);
+                backgroundWavePath.quadTo(backgroundWaveStartX + perWaveWidth / 4 * 3, waveY +
+                        backgroundWaveHeightOffset, backgroundWaveStartX + perWaveWidth, waveY);
+            }
+        }
+        if (drawBackgroundWave) {
+            backgroundWavePath.addRect(new RectF(backgroundWaveStartXOffset, waveY, (float) (getWidth() * 1.5),
+                    getHeight()), Path.Direction.CW);
+            backgroundWavePath.close();
+            canvas.drawPath(backgroundWavePath, backgroundWavePaint);
         }
         foregroundWavePath.addRect(new RectF(foregroundWaveStartXOffset, waveY, (float) (getWidth() * 1.5), getHeight
                 ()), Path.Direction.CW);
-        backgroundWavePath.addRect(new RectF(backgroundWaveStartXOffset, waveY, (float) (getWidth() * 1.5), getHeight
-                ()), Path.Direction.CW);
-
         foregroundWavePath.close();
-        backgroundWavePath.close();
-
-        canvas.drawPath(backgroundWavePath, backgroundWavePaint);
         canvas.drawPath(foregroundWavePath, foregroundWavePaint);
     }
 
@@ -211,36 +210,86 @@ public class WaveView extends View {
         foregroundWaveBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         foregroundWaveBitmap.setHasAlpha(true);
         foregroundWaveCanvas = new Canvas(foregroundWaveBitmap);
-        if (backgroundWaveBitmap != null) {
-            backgroundWaveBitmap.recycle();
+        if (drawBackgroundWave) {
+            if (backgroundWaveBitmap != null) {
+                backgroundWaveBitmap.recycle();
+            }
+            backgroundWaveBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            backgroundWaveBitmap.setHasAlpha(true);
+            backgroundWaveCanvas = new Canvas(backgroundWaveBitmap);
         }
-        backgroundWaveBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        backgroundWaveBitmap.setHasAlpha(true);
-        backgroundWaveCanvas = new Canvas(backgroundWaveBitmap);
-
         float circleCenterX = getWidth() / 2;
         float circleCenterY = getHeight() * lineMarginTop + lineWidth / 2;
         float circleRadius = getWidth() * outerHalfCircleRadius;
 
         foregroundWaveCanvas.drawArc(new RectF(circleCenterX - circleRadius, circleCenterY - circleRadius,
                 circleCenterX + circleRadius, circleCenterY + circleRadius), -180, 180, true, foregroundWavePaint);
-        backgroundWaveCanvas.drawArc(new RectF(circleCenterX - circleRadius, circleCenterY - circleRadius,
-                circleCenterX + circleRadius, circleCenterY + circleRadius), -180, 180, true, backgroundWavePaint);
-
         foregroundWavePaint.setShader(new BitmapShader(foregroundWaveBitmap, Shader.TileMode.CLAMP, Shader.TileMode
                 .CLAMP));
-        backgroundWavePaint.setShader(new BitmapShader(backgroundWaveBitmap, Shader.TileMode.CLAMP, Shader.TileMode
-                .CLAMP));
+        if (drawBackgroundWave) {
+            backgroundWaveCanvas.drawArc(new RectF(circleCenterX - circleRadius, circleCenterY - circleRadius,
+                    circleCenterX + circleRadius, circleCenterY + circleRadius), -180, 180, true, backgroundWavePaint);
+            backgroundWavePaint.setShader(new BitmapShader(backgroundWaveBitmap, Shader.TileMode.CLAMP, Shader.TileMode
+                    .CLAMP));
+        }
 
         resetWaveStartXOffset(true, true);
+        startAnim();
+    }
+
+    private ValueAnimator foregroundAnim;
+    private ValueAnimator backgroundAnim;
+
+    public void startAnim() {
+        if (foregroundAnim == null) {
+            foregroundAnim = ValueAnimator.ofInt(foregroundWaveStartXOffset, 0);
+            foregroundAnim.setDuration(2000);
+            foregroundAnim.setInterpolator(new LinearInterpolator());
+            foregroundAnim.setRepeatCount(ValueAnimator.INFINITE);
+            foregroundAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    foregroundWaveStartXOffset = (int) animation.getAnimatedValue();
+                    invalidate();
+                }
+            });
+        }
+        foregroundAnim.start();
+        if (drawBackgroundWave) {
+            if (backgroundAnim == null) {
+                backgroundAnim = ValueAnimator.ofInt(backgroundWaveStartXOffset, 0);
+                backgroundAnim.setDuration(3000);
+                backgroundAnim.setInterpolator(new LinearInterpolator());
+                backgroundAnim.setRepeatCount(ValueAnimator.INFINITE);
+                backgroundAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        backgroundWaveStartXOffset = (int) animation.getAnimatedValue();
+                        invalidate();
+                    }
+                });
+            }
+            backgroundAnim.start();
+        }
     }
 
     private void resetWaveStartXOffset(boolean resetForeground, boolean resetBackground) {
         if (resetForeground) {
-            foregroundWaveStartXOffset = -getWidth() / waveCount * 3;
+            foregroundWaveStartXOffset = (int) (-perWaveWidth * 3);
         }
-        if (resetBackground) {
-            backgroundWaveStartXOffset = -getWidth() / waveCount * 4;
+        if (resetBackground && drawBackgroundWave) {
+            backgroundWaveStartXOffset = (int) (-perWaveWidth * 3);
         }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        if (foregroundAnim != null) {
+            foregroundAnim.cancel();
+        }
+        if (backgroundAnim != null) {
+            backgroundAnim.cancel();
+        }
+        super.onDetachedFromWindow();
     }
 }
